ce45cb2915c159389bd04fc4083fd4a9af250c30
Priyanshu Singh <87827548+priyanshusingh-in@users.noreply.github.com>
2022-02-16 20:53:26 +0530

    Create maxAndMin.java

---
+++ PROGRAMS/maxAndMin.java
@@ -0,0 +1,12 @@
+//Program to find minimum and maximum value of any datype.
+
+public class maxAndMin{
+    public static void main(String[] args) 
+    {
+     int min = Integer.MIN_VALUE;
+     int max = Integer.MAX_VALUE;
+     
+     System.out.println(min);
+     System.out.println(max); 
+    }
+}
