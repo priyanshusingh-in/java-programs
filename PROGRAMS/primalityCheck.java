41bb2147de2bb8e1bc065c4392aff9ee704382ce
Priyanshu Singh <87827548+priyanshusingh-in@users.noreply.github.com>
2022-02-22 13:41:14 +0530

    Update primalityCheck.java

--- PROGRAMS/primalityCheck.java
+++ PROGRAMS/primalityCheck.java
@@ -6,23 +6,28 @@ class primalityCheck
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int div = 2;
-		boolean isPrime = true;
-		
-		while(div <= n-1)
-    {
-		    
-		    if(n%div == 0)
-        {
-		    isPrime = false;
-		     }
-		     div = div+1;
-		}
-	if(isPrime)
-	 System.out.println("Prime");
-	else
-	 System.out.println("Not prime");
+	  Scanner sc = new Scanner(System.in);
+	  int n = sc.nextInt();
+	  int div = 2;
+	  boolean isPrime = true;
+	  
+	  while(div <= n/2)
+	  {
+	      if(n%div == 0)
+	      {
+	          isPrime = false;
+	      }
+	      div += 1;
+	  }
+	 
+	  if(isPrime)
+	  {
+	      System.out.println("Prime");
+	  }
+	  else
+	  {
+	      System.out.println("Composite");
+	  }
+	
 	}
 }
