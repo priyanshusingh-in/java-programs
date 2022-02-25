cb11cab09cc669e29eefbe6a43a0070cc5e10131
Priyanshu Singh <in.priyanshusingh@gmail.com>
2022-02-25 06:57:40 +0530

    Create squarePattern3.java

---
+++ PROGRAMS/squarePattern3.java
@@ -0,0 +1,33 @@
+/*
+
+Sample input:
+
+4
+
+Sample output:
+
+1234
+1234
+1234
+1234
+
+*/
+
+class squarePattern3
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int i = 1;
+		while(i<=n){
+		    int j = 1;
+		    while(j<=n){
+		        System.out.print(j);
+		        j++;
+		    }
+		    System.out.println();
+		    i++;
+		}
+	}
+}
