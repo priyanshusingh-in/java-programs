48d901ce42a8df6b678ab43e99f7ecc68aa526f2
Priyanshu Singh <in.priyanshusingh@gmail.com>
2022-02-26 06:17:13 +0530

    Create trianglePattern2.java

---
+++ PROGRAMS/trianglePattern2.java
@@ -0,0 +1,35 @@
+/*
+
+Sample input:
+4
+
+Sample output:
+1
+23
+456
+78910
+
+*/
+
+import java.util.*;
+
+class trianglePattern2
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int p = 1;
+		int i = 1;
+		while(i<=n){
+		    int j = 1;
+		    while(j<=i){
+		        System.out.print(p);
+		        p++;
+		        j++;
+		    }
+		    System.out.println();
+		    i++;
+		}
+	}
+}
