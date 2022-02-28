8de0e9ddfcde4f17baf0e4687840f7b19de44ac0
Priyanshu Singh <in.priyanshusingh@gmail.com>
2022-02-28 21:27:20 +0530

    Create characterPattern2Alpha.java

---
+++ PROGRAMS/characterPattern2Alpha.java
@@ -0,0 +1,33 @@
+/*
+
+Sample input:
+3
+
+Sample Output:
+A
+BB
+CCC
+
+*/
+
+import java.util.Scanner;
+public class characterPattern2Alpha {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+        int i = 1;
+        while(i<=N){
+            int j = 1;
+            while(j<=i){
+                char ithchar = (char)('A'+i-1);
+                System.out.print(ithchar);
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+		
+	}
+
+}
