720471b44d9d8b6eb3be32f9f5391de494cb3c7c
Priyanshu Singh <in.priyanshusingh@gmail.com>
2022-02-28 08:23:53 +0530

    Create characterPattern1.java

---
+++ PROGRAMS/characterPattern1.java
@@ -0,0 +1,33 @@
+/*
+
+Sample input:
+4
+
+Sample output:
+ABCD
+ABCD
+ABCD
+ABCD
+
+*/
+
+import java.util.Scanner;
+public class characterPattern1 {
+    public static void main(String args[]) {
+     Scanner s=new Scanner(System.in);
+     int n=s.nextInt();
+     int i=1;
+     while(i<=n){
+         
+         int j=1;
+         while(j<=n){
+             char jthchar=(char)('A'+j-1);
+             System.out.print(jthchar);
+             
+             j=j+1;
+         }
+         System.out.println();
+         i=i+1;
+     }
+    }
+}
