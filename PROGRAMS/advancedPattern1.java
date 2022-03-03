57221b2b82718dc3311c76cacf02d5f6b9e96bb2
Priyanshu Singh <in.priyanshusingh@gmail.com>
2022-03-03 19:50:31 +0530

    Create advancedPattern1.java

---
+++ PROGRAMS/advancedPattern1.java
@@ -0,0 +1,41 @@
+/*
+
+Sample input:
+4
+
+Sample output:
+12344321
+123**321
+12****21
+1******1
+
+*/
+        
+        import java.util.Scanner;
+        
+        class advancedPattern1
+        {
+        	public static void main (String[] args) throws java.lang.Exception
+        	{
+        		Scanner sc = new Scanner(System.in);
+        		int n = sc.nextInt();
+        		int i = 1;
+        		while(i<=n){
+        		    int j = 1;
+        		    while(j<=2*n){
+        		        if(j<=n+1-i||j>=n+i){
+        		         if(j<=n){
+        		            System.out.print(j);
+        		        }
+        		            else
+        		            System.out.print((n*2+1)-j);
+        		        }
+        		        else
+        		            System.out.print("*");
+        		        j++;
+        		    }
+        		    System.out.println();
+        		    i++;
+        		}
+        	}
+        }
