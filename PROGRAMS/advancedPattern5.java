64bcb554a8c89a7f53e82041b52f3ce0ada996a9
Priyanshu Singh <in.priyanshusingh@gmail.com>
2022-03-09 16:16:17 +0530

    Update advancedPattern5.java
    Optimised the code.

--- PROGRAMS/advancedPattern5.java
+++ PROGRAMS/advancedPattern5.java
@@ -24,16 +24,15 @@ class advancedPattern5
         int n = scanner.nextInt();
         for (int i = 1; i <= n ; i++)
         {
-            for (int j = 1; j < n-i+1; j++)
+            for (int j = 1; j <= n-i; j++)
             {
                 System.out.print(" ");
             }
-
-            for (int k = n-i; k < n; k++)
+            for (int k = 1; k <= i; k++)
             {
                 System.out.print("*");
             }
             System.out.println();
         }
- 	}
+  }
 }
