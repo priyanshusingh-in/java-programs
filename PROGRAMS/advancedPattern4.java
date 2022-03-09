53a960c785f4afa4a1425dddada4db13619991ad
Priyanshu Singh <in.priyanshusingh@gmail.com>
2022-03-09 08:21:12 +0530

    Create advancedPattern4.java
    unfinished code.

---
+++ PROGRAMS/advancedPattern4.java
@@ -0,0 +1,32 @@
+/*
+
+Sample input:
+
+
+*/
+
+
+import java.util.*;
+
+class advancedPattern4
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        for (int i = 1dd; i <= n ; i++)
+        {
+            for (int j = i; j < n; j++)
+            {
+                System.out.print(j);
+            }
+
+            for (int k = n - i; k < n; k++)
+            {
+                System.out.print(5);
+            }
+            System.out.println();
+        }
+	}
+}
