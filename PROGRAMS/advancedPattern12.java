627225041cb7b86ccecb935e8f14275afd170219
Priyanshu Singh <in.priyanshusingh@gmail.com>
2022-03-13 11:22:12 +0530

    Create advancedPattern12.java

---
+++ PROGRAMS/advancedPattern12.java
@@ -0,0 +1,67 @@
+/*
+
+Print the following pattern for the given number of rows.
+Note: N is always odd.
+
+
+Pattern for N = 5
+
+
+
+The dots represent spaces.
+
+
+
+Input format :
+N (Total no. of rows and can only be odd)
+Output format :
+Pattern in N lines
+Constraints :
+1 <= N <= 49
+Sample Input 1:
+5
+Sample Output 1:
+  *
+ ***
+*****
+ ***
+  *
+Sample Input 2:
+3
+Sample Output 2:
+  *
+ ***
+  *
+
+*/
+
+import java.util.Scanner;
+public class advancedPattern12 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N=sc.nextInt();
+        int firstHalf = (N+2)/2;
+        int secondHalf = firstHalf - 1;
+
+        for(int i=1; i<=firstHalf; i++){
+            for(int j=1; j<=firstHalf-i; j++){
+                System.out.print(' ');
+            }
+            for(int k=1; k<=(2*i)-1; k++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+
+        for(int i=secondHalf; i>=1; i--){
+            for(int j=1; j<=(secondHalf-i)+1;j++){
+                System.out.print(' ');
+            }
+            for(int k=1; k<=(2*i)-1; k++){
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+}
