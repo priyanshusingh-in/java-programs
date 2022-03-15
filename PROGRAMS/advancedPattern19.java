6fbbb846d72dd07f4ed73de69b7b51b37070e443
Priyanshu Singh <in.priyanshusingh@gmail.com>
2022-03-15 07:27:52 +0530

    Create advancedPattern19.java

---
+++ PROGRAMS/advancedPattern19.java
@@ -0,0 +1,45 @@
+/*
+
+Write a program to print triangle of user defined integers sum.
+Input Format :
+A single integer, N
+Output Format :
+Required Pattern
+Constraints :
+0 <= N <= 50
+Sample Input 1 :
+3
+Sample Output 1 :
+1=1
+1+2=3
+1+2+3=6
+Sample Input 2 :
+ 5
+Sample Output 2 :
+1=1
+1+2=3
+1+2+3=6
+1+2+3+4=10
+1+2+3+4+5=15
+
+*/
+
+import java.util.Scanner;
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        
+        int n=sc.nextInt();
+        int sum=0;
+        int i,j;
+        for( i=1; i<=n; i++){
+            sum+=i;
+            for( j=1; j<=i-1; j++){
+                System.out.print(j+"+");
+            }
+            System.out.print(j);
+            System.out.println("="+sum);
+        }
+	}
+}
