c39dfcd5b5a465d6bd3ed3f7c89f341bea39e5ac
Priyanshu Singh <in.priyanshusingh@gmail.com>
2022-04-01 06:52:05 +0530

    Create calculateNcR.java

---
+++ PROGRAMS-2/calculateNcR.java
@@ -0,0 +1,43 @@
+/*
+
+Java code to calculate NcR.
+
+Sample input:
+6
+4
+
+Sample output:
+15
+
+*/
+
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+class calculateNcR
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int r=sc.nextInt();
+		
+		int factN=1;
+		for(int i=1;i<=n;i++)
+		{
+		  factN*=i;
+		}
+		int factR=1;
+		for(int i=1;i<=r;i++)
+		{
+		    factR*=i;
+		}
+		int factNR=1;
+		for(int i=1;i<=n-r;i++)
+		{
+		 factNR*=i;
+		}
+		int result=factN/(factR*factNR);
+		System.out.println(result);
+	}
+}
