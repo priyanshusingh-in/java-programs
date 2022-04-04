e2aa6f4dd23a9027a5fe531bbadd11dd0da1c7a7
Priyanshu Singh <in.priyanshusingh@gmail.com>
2022-04-04 11:25:18 +0530

    Create checkPrime.java

---
+++ PROGRAMS-2/checkPrime.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class checkPrime
+{
+	public static boolean primalityCheck(int num)
+  {
+   int div = 2;
+	  
+	  while(div <= n/2)
+	  {
+	      if(n%div == 0)
+	      {
+    		return false;
+	      }
+	      div += 1;
+	  }
+    return true;	}
+  }
+  
+  public static void main (String[] args) throws java.lang.Exception
+	{
+	  Scanner sc = new Scanner(System.in);
+	  int n = sc.nextInt();
+	  co
+}
