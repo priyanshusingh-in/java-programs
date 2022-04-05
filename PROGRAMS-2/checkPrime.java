5fb94912e6024f0a3b63792b8ce60e21db2d8b2e
Priyanshu Singh <in.priyanshusingh@gmail.com>
2022-04-05 11:09:52 +0530

    Update checkPrime.java

--- PROGRAMS-2/checkPrime.java
+++ PROGRAMS-2/checkPrime.java
@@ -2,24 +2,26 @@ import java.util.*;
 
 class checkPrime
 {
-	public static boolean primalityCheck(int num)
+  public static boolean primalityCheck(int num)
   {
    int div = 2;
 	  
-	  while(div <= n/2)
+	  while(div <= num/2)
 	  {
-	      if(n%div == 0)
+	      if(num%div == 0)
 	      {
     		return false;
 	      }
 	      div += 1;
 	  }
-    return true;	}
+    return true;
   }
   
-  public static void main (String[] args) throws java.lang.Exception
+       public static void main (String[] args) throws java.lang.Exception
 	{
 	  Scanner sc = new Scanner(System.in);
 	  int n = sc.nextInt();
-	  co
+	  boolean result=primalityCheck(n);
+	  System.out.println(result);
+       }
 }
